81370692a08b4e5a364cffccc0a9976565b452d9
anilkamber <105453976+anilkamber@users.noreply.github.com>
2025-10-01 04:29:14 +0000

    Deploying to gh-pages from @ anilkamber/anilkamber.github.io@4f548f3666b8534c46f1567ef0959e42bcd1270f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,9 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-anıl-kamber-br-small-i-this-website-is-under-construction-i-small",
-    title: "Anıl Kamber <br>
-<small><i>*This website is under construction*</i></small>",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
